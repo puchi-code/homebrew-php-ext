0c78157b441242a204a46ac0adfc8c9a304542bd
Karol Puchalski <karol.puchalski@ngroup.pl>
2021-01-07 04:02:21 +0100

    pgsql formula fix

--- Formula/php-pgsql.rb
+++ Formula/php-pgsql.rb
@@ -2,5 +2,6 @@ require_relative "../lib/php_extension_formula"
 
 class PhpPgsql < PhpExtensionFormula
   extension_dsl "Psql"
-  instantclient_options arg: "--with-pgsql"
+  configure_arg %W[
+    --with-pgsql
 end
