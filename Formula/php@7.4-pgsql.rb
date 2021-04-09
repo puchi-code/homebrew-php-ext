3ae620b5ad1e846cb41d8ab34d219a4716f5e4c3
puchi-code <karol.puchalski@poczta.fm>
2021-04-10 01:29:31 +0200

    Update php@7.4-pgsql.rb

--- Formula/php@7.4-pgsql.rb
+++ Formula/php@7.4-pgsql.rb
@@ -3,6 +3,6 @@ require_relative "../lib/php_extension_formula"
 class PhpAT74Pgsql < PhpExtensionFormula
   extension_dsl "Psql"
   configure_arg %W[
-    --with-pgsql
+    --with-pgsql=/opt/homebrew/Cellar/postgresql/13.2_1/bin
   ]
 end
